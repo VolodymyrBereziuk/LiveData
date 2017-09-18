c0b10387f19e92504d63b95ad9e6b274e306e2e3
Volodymyr Bereziuk <volodymyr.bereziuk@lemberg.co.uk>
2017-09-18 21:30:52 +0300

    Initial commit.

---
+++ app/src/main/java/livedata/vb/com/MainActivity.java
@@ -0,0 +1,16 @@
+package livedata.vb.com;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import livedata.vb.com.livedata.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
